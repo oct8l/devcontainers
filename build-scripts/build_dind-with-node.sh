a5df7b77f89e4905f5329369a69bf0f5f355399d
oct8l <git@mail.oct8l.com>
2024-04-21 20:48:25 -0500

    Fix the shell scripts to not try a multiarch build

--- build-scripts/build_dind-with-node.sh
+++ build-scripts/build_dind-with-node.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 devcontainer build --workspace-folder ./dind-with-node --image-name oct8l/devcontainers:amd64_dind-latest &
-devcontainer build --workspace-folder ./dind-with-node --platform linux/arm64 --image-name oct8l/devcontainers:arm64_dind-latest &
+#devcontainer build --workspace-folder ./dind-with-node --platform linux/arm64 --image-name oct8l/devcontainers:arm64_dind-latest &
 
 # Wait for the builds to finish
 wait
 
 docker push oct8l/devcontainers:amd64_dind-latest &
-docker push oct8l/devcontainers:arm64_dind-latest &
+#docker push oct8l/devcontainers:arm64_dind-latest &
 
 # Wait until both images are pushed
 wait
 
 # Create and push our custom manifest
-docker manifest create oct8l/devcontainers:dind-latest --amend oct8l/devcontainers:amd64_dind-latest --amend oct8l/devcontainers:arm64_dind-latest
+docker manifest create oct8l/devcontainers:dind-latest && \
+#    --amend oct8l/devcontainers:arm64_dind-latest  && \
+    --amend oct8l/devcontainers:amd64_dind-latest
 docker manifest push oct8l/devcontainers:dind-latest
