c8b8bcd4a496b7fc1979e7c53810505d359625f1
oct8l <git@mail.oct8l.com>
2024-04-21 20:40:07 -0500

    Try some different pipeline methods to maybe build a manifest for multiple architects

--- build-scripts/build_python-selenium.sh
+++ build-scripts/build_python-selenium.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-devcontainer build --workspace-folder ./python-selenium --image-name oct8l/devcontainers:python-selenium-latest
+devcontainer build --workspace-folder ./python-selenium --image-name oct8l/devcontainers:amd64_python-selenium-latest &
+devcontainer build --workspace-folder ./python-selenium --platform linux/arm64 --image-name oct8l/devcontainers:arm64_python-selenium-latest &
 
-docker push oct8l/devcontainers:python-selenium-latest
+# Wait for the builds to finish
+wait
+
+docker push oct8l/devcontainers:amd64_python-selenium-latest &
+docker push oct8l/devcontainers:arm64_python-selenium-latest &
+
+# Wait until both images are pushed
+wait
+
+# Create and push our custom manifest
+docker manifest create oct8l/devcontainers:python-selenium-latest --amend oct8l/devcontainers:amd64_python-selenium-latest --amend oct8l/devcontainers:arm64_python-selenium-latest
+docker manifest push oct8l/devcontainers:python-selenium-latest
