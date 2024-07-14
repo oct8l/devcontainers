1e3860cdaf1825eb6e94c8e4570049fba5c60eee
oct8l <git@mail.oct8l.com>
2024-07-13 22:57:57 -0500

    Fix multi arch scripts now that it will work with the new base images

--- build-scripts/build_python-selenium.sh
+++ build-scripts/build_python-selenium.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+# Start the builds in the background
 devcontainer build --workspace-folder ./python-selenium --image-name oct8l/devcontainers:amd64_python-selenium-latest &
-#devcontainer build --workspace-folder ./python-selenium --platform linux/arm64 --image-name oct8l/devcontainers:arm64_python-selenium-latest &
+devcontainer build --workspace-folder ./python-selenium --platform linux/arm64 --image-name oct8l/devcontainers:arm64_python-selenium-latest &
 
-## Wait for the builds to finish
-#wait
+# Wait for the builds to finish
+wait
 
+# Push the images in the background
 docker push oct8l/devcontainers:amd64_python-selenium-latest &
-#docker push oct8l/devcontainers:arm64_python-selenium-latest &
+docker push oct8l/devcontainers:arm64_python-selenium-latest &
 
-## Wait until both images are pushed
-#wait
-#
-## Create and push our custom manifest
-#docker manifest create oct8l/devcontainers:python-selenium-latest && \
-#    --amend oct8l/devcontainers:arm64_python-selenium-latest && \
-#    --amend oct8l/devcontainers:amd64_python-selenium-latest
-#docker manifest push oct8l/devcontainers:python-selenium-latest
+# Wait until both images are pushed
+wait
+
+# Create and push our custom manifest
+docker manifest create oct8l/devcontainers:python-selenium-latest \
+    --amend oct8l/devcontainers:arm64_python-selenium-latest \
+    --amend oct8l/devcontainers:amd64_python-selenium-latest
+docker manifest push oct8l/devcontainers:python-selenium-latest
